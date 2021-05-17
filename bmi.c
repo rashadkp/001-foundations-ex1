9d4efbefdc23effbdbce7a49629c91d42036f150
rashadkp <63586227+rashadkp@users.noreply.github.com>
2021-05-17 22:14:51 +0530

    trial

---
+++ bmi.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <cs50.h>
+int main(){
+    float a=get_int("Enter your name (in m)");
+    float b=get_int("Enter your weight(in Kg) ");
+    float c= a/(b*b);
+    if c<18.5
+      printf(a c "underweight")
+
+}
